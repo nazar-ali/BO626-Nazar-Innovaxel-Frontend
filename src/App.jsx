3a48b938991ed9497b05f0995763a3ab02fbe926
nazar_ali <nazaralibaloch3@gmail.com>
2026-06-08 17:16:24 +0500

    Initial project setup with Vite, React, JavaScript, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+      <dev>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </dev>
+    </>
+  )
+}
+
+export default App
