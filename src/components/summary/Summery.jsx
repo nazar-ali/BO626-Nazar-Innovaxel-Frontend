4c4036ceda32b035930647a278e163c5c30e244e
nazar_ali <nazaralibaloch3@gmail.com>
2026-06-10 14:02:08 +0500

    fix build vercel build error v3

--- src/components/summary/Summery.jsx
+++ src/components/summary/Summery.jsx
@@ -1,6 +1,6 @@
 import SummeryCards from "./SummeryCards";
 import { useExpenses } from "../../context/useExpenses";
-import CategoryPieChart from "./CategoryPiehart";
+import CategoryPieChart from "./CategoryPiechart";
 import MonthlyTrendBarChart from "./MonthlyTrendBarChart";
 
 export default function Summary() {
