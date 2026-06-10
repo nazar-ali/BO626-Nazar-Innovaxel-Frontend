7e17f6b80a0ce90cd53eb55dc68a1b223316a142
nazar_ali <nazaralibaloch3@gmail.com>
2026-06-10 14:16:27 +0500

    fix build vercel build error v4

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,14 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  
 })
